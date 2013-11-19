31d2df6b867e931a3e0f6359ae4edbaa25f5216d
ghost-00 <jos.zigabe@student.uclouvain.begit config --global user.email jos.zigabe@student.uclouvain.be>
2013-11-19 20:37:31 +0100

    projet

---
+++ MINIX/MINIX/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MINIX
+//
+//  Created by Jos Zigabe on 19/11/13.
+//  Copyright (c) 2013 Jos Zigabe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
